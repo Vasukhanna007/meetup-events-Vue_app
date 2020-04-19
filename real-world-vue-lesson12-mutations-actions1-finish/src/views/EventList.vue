5f75783eac719db11a0a86e8d999c357834c9ef8
Vasu Khanna <vasu.khanna@mail.mcgill.ca>
2020-04-19 09:22:22 +0530

    mutation and action part 2 finished

---
+++ real-world-vue-lesson12-mutations-actions1-finish/src/views/EventList.vue
@@ -0,0 +1,39 @@
+ <template>
+  <div>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">Prev Page</router-link>|
+    </template>
+    <template v-if="hasNextPage">
+      <router-link :to="{ name: 'event-list', query: { page: page + 1 } }">Next Page</router-link>
+    </template>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
+export default {
+  components: {
+    EventCard
+  },
+
+  created() {
+    this.perPage = 3
+    this.$store.dispatch('fetchEvents', {
+      perPage: this.perPage,
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      // What page we're currently on
+      return parseInt(this.$route.query.page) || 1
+    },
+    hasNextPage() {
+      return this.eventsTotal > this.page * this.perPage
+    },
+    ...mapState(['events', 'eventsTotal'])
+  }
+}
+</script>
